910b467964110c44422d2b517c181ac221b25f7c
Bruno Bulic <brbulic@gmail.com>
2013-07-15 22:09:36 +0200

    modifications for new model

--- hosts_genhar_uas.js
+++ hosts_genhar_uas.js
@@ -35,6 +35,26 @@ function attachPreviousEntries(prevEntries, currEntries) {
     });
 }
 
+function propertyExists(stringy) {
+    return (typeof stringy !== 'undefined' && stringy !== null && stringy.length > 0);
+}
+
+function marlinHeadersSetup(userAppConfig) {
+    var finish = [];
+
+    if (propertyExists(userAppConfig.appOS)) {
+        finish.push(userAppConfig.appOS, ' ');
+    }
+    if (propertyExists(userAppConfig.appVersion)) {
+        finish.push(userConfig.appVersion, ' ');
+    }
+    if (propertyExists(userAppConfig.appDate)) {
+        finish.push(userConfig.appDate, ' ');
+    }
+
+    return finish.join('').trim();
+}
+
 function filenameMapper(filenamePrefix, filename) {
     'use strict';
 
@@ -143,6 +163,7 @@ var fs = require('fs');
 var startingAddress = null,
     redirectAddress = null,
     userAgentProfile = null,
+    userConfig = null,
     isRedirect = null;
 
 var previousEntries     = [];
@@ -161,8 +182,12 @@ var renderAndMeasurePage = function(measuredUrl) {
         page.settings.userAgent = userAgentProfile;
     }
 
-    page.customHeaders = {
-        'HTTP_X_MARLIN_MOBILE' : page.settings.userAgent
+    var result = marlinHeadersSetup(userConfig);
+
+    if (result != null) {
+        page.customHeaders = {
+            'HTTP_X_MARLIN_MOBILE' : result
+        };    
     };
 
     page.onLoadStarted = function () {
@@ -283,7 +308,12 @@ if (system.args.length === 1) {
 
     var argsLength = system.args.length;
     if (argsLength === 3) {
-        userAgentProfile = system.args[2];
+        userConfig = JSON.parse(system.args[2]);
+        if (typeof userConfig.userAgent !== 'undefined') {
+            userAgentProfile = userConfig.userAgent;
+        };
+
+        console.log("I a user config:\n" + system.args[2]);
     }
 
     renderAndMeasurePage(startingAddress);
